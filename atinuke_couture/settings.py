868e38614f639ef9507081c79cc43563976837dd
andrew samuel <samuel.andrew5804@gmail.com>
2024-03-22 11:45:37 +0100

    changes debug

--- atinuke_couture/settings.py
+++ atinuke_couture/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DEBUG = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -130,7 +131,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-DEBUG = False
 
 
 
