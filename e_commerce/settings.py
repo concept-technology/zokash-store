7599d070b3a23dacdf388d3dc4bfc95ad5a772aa
andrew samuel <samuel.andrew5804@gmail.com>
2024-07-03 02:24:08 +0100

    added the order adress

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -149,21 +149,21 @@ PAYSTACK_PUBLIC_KEY = 'pk_test_ea75ece062f2a82f8f18506dbeff5f085a1ca58a'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-     }
-}
-
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         # Replace this value with your local database's connection string.
-#         default='postgres://zokash_household_user:PQJ6jgHPGFjzS3zV3vmhGxRTevURRevd@dpg-cpojj7mehbks73ekcb7g-a.oregon-postgres.render.com/zokash_household',
-#         conn_max_age=600,
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#      }
 # }
+
+
+DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://zokash_household_user:PQJ6jgHPGFjzS3zV3vmhGxRTevURRevd@dpg-cpojj7mehbks73ekcb7g-a.oregon-postgres.render.com/zokash_household',
+        conn_max_age=600,
+    )
+}
 
 
 AUTH_PASSWORD_VALIDATORS = [
