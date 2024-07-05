318ad2cf44b68738f0b3c5a11d48521e38cc37ef
andrew samuel <samuel.andrew5804@gmail.com>
2024-07-05 20:22:31 +0100

    UPDATED DEBUG

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -10,7 +10,7 @@ load_dotenv()
 SECRET_KEY = os.environ.get("SECRET_KEY", 'your_default_secret_key')
 
 # DEBUG = os.environ.get("DEBUG", 'True').lower() in ['true', '1', 't']
-DEBUG=True
+DEBUG=False
 ALLOWED_HOSTS = ['*', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
